b6d323c9539148044bcad613c5d51764a10e4707
GabrielDesktop <gabrielpan295@gmail.com>
2025-12-18 15:04:09 -0600

    button event handling added, and updating UI with actual card objects

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 class Deck {
     cards = [];
     suites = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
-    numbers = ["A",2,3,4,5,6,7,8,9,10,'J','Q','K'];
+    numbers = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
 
     constructor(){
         this.initCards();
         this.shuffleDeck();
+        Card.amountMade = 0;
     }
   
     initCards(){
@@ -35,15 +36,30 @@ class Deck {
 }
 
 class Card {
-    number = "";
+    number = 0;
     suit = "";
-    url = 0;
+    url = 0; //creates deck of cards with corresponding image file name
+    static amountMade = 0; //tracks the number of cards made to map to image name
 
     constructor(number, suit) {
         this.number = number;
         this.suit = suit;
-        this.url += 1;
+        Card.amountMade += 1;
+        this.url = Card.amountMade;
     }
+
+    recalculateFaceCard() {
+        switch (this.number) {
+            case 'J':
+            case 'Q':
+            case 'K':
+                return 10;
+            case 'A':
+                return 1;
+            default:
+                return this.number;
+    }
+  }
 }
 
 class Player { 
@@ -60,25 +76,56 @@ class Player {
     this.hand.push(card);
   }
 
+  getTotalInHand() {
+    var total = 0;
+    for(var card of this.hand) {
+        var num = card.recalculateFaceCard();
+        total += num;
+    }
+    return total;
+  }
+
 }
 
-function gameRound() {
+function initGame(player1, player2) {
     var myDeck = new Deck();
-    var player1 = new Player('Player 1');
-    var player2 = new Player('Player 2');
     player1.addToHand(myDeck);
     player1.addToHand(myDeck);
     player1.addToHand(myDeck);
     player2.addToHand(myDeck);
     player2.addToHand(myDeck);
     player2.addToHand(myDeck);
+}
+
+function updateCardUI(player, htmlId) {
+    for(var i = 1; i <= 3; i++) {
+        var num = player.hand[i - 1].url; //retrieves corresponding url of card in players hand
+        var imgTag = document.getElementById(htmlId + i);
+        imgTag.setAttribute('src', 'images/card/' + num + '.png');
+    }
 }
 
 function onButton() {
-    document.getElementById("match-summary").innerHTML = "It works!!!";
+    var player1 = new Player('Player 1');
+    var player2 = new Player('Player 2');
+    initGame(player1, player2);
+    updateCardUI(player1, "player1-card");
+    updateCardUI(player2, "player2-card");
+
+    var p1Total = player1.getTotalInHand();
+    var p2Total = player2.getTotalInHand();
+    var summary = `${player1.name} has a total score of ${p1Total}. ${player2.name} has a total score of ${p2Total}. `;
+    if (p1Total > p2Total) {
+        summary += `${player1.name} wins!`;
+    }
+    else if (p1Total < p2Total) {
+        summary += `${player2.name} wins!`;
+    }
+    else {
+        summary += `It's a tie!`;
+    }
+    document.getElementById("match-summary").innerHTML = summary;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    var button = document.querySelector(".shuffle-button");
-    button.addEventListener("click", onButton);
-});
+var button = document.querySelector(".shuffle-button");
+button.addEventListener("click", onButton);
